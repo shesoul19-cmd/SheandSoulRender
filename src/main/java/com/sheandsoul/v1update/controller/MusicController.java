27b306fb36c94bd84aa2c8f6bc4dde26a7f26870
nitish-11k <nitishk0014@gmail.com>
2025-09-20 14:47:59 +0530

    Music Fixed

--- src/main/java/com/sheandsoul/v1update/controller/MusicController.java
+++ src/main/java/com/sheandsoul/v1update/controller/MusicController.java
@@ -1,11 +1,19 @@
 package com.sheandsoul.v1update.controller;
 
-import com.sheandsoul.v1update.dto.MusicDto;
-import com.sheandsoul.v1update.services.MusicService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sheandsoul.v1update.dto.MusicDto;
+import com.sheandsoul.v1update.entities.User;
+import com.sheandsoul.v1update.services.MusicService;
+import com.sheandsoul.v1update.services.MyUserDetailService;
 
 @RestController
 @RequestMapping("/api/v1/music")
@@ -14,8 +22,12 @@ public class MusicController {
     @Autowired
     private MusicService musicService;
 
+    @Autowired
+    private MyUserDetailService userDetailsService;
+
     @GetMapping("/get")
-    public List<MusicDto> getAllMusic() {
+    public List<MusicDto> getAllMusic(Authentication authentication) {
+        User currentUser = userDetailsService.findUserByEmail(authentication.getName());
         return musicService.getAllMusic();
     }
 
