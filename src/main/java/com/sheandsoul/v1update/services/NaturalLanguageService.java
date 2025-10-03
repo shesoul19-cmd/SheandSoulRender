5a965e7b38be2a8aa314427eec14822c69a11aee
nitish-11k <nitishk0014@gmail.com>
2025-10-03 19:32:08 +0530

    fixing the NLP

--- src/main/java/com/sheandsoul/v1update/services/NaturalLanguageService.java
+++ src/main/java/com/sheandsoul/v1update/services/NaturalLanguageService.java
@@ -11,11 +11,11 @@ public class NaturalLanguageService {
         }
         // This is a simple implementation that replaces markdown with HTML.
         // More complex NLP tasks can be added here in the future.
-        return rawResponse
-            .replaceAll("\\*\\*\\*(.*?)\\*\\*\\*", "<strong><em>$1</em></strong>")
-            .replaceAll("\\*\\*(.*?)\\*\\*", "<strong>$1</strong>")
-            .replaceAll("\\*(.*?)\\*", "<em>$1</em>")
-            .replaceAll("##\\s*(.*?)(?=\\n|$)", "<h2>$1</h2>")
-            .replaceAll("\\n", "<br/>");
+        return rawResponse.trim();
+            // .replaceAll("\\*\\*\\*(.*?)\\*\\*\\*", "<strong><em>$1</em></strong>")
+            // .replaceAll("\\*\\*(.*?)\\*\\*", "<strong>$1</strong>")
+            // .replaceAll("\\*(.*?)\\*", "<em>$1</em>")
+            // .replaceAll("##\\s*(.*?)(?=\\n|$)", "<h2>$1</h2>")
+            // .replaceAll("\\n", "<br/>");
     }
 }
